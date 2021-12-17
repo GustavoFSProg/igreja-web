af31348efbc8dd71ed1953b3618562407d21c77c
Gustavo <gustavo.prog40@gmail.com>
2021-12-17 11:09:59 -0300

    layout

--- src/App.js
+++ src/App.js
@@ -5,7 +5,36 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={Logo} alt="logo" style={{ marginLeft: '48px' }} />
-        <hr style={{ marginTop: '40px' }} />
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: '2px',
+          }}
+        >
+          <ul
+            style={{
+              listStyle: 'none',
+              display: 'flex',
+              justifyItems: 'center',
+              alignItems: 'center',
+              color: '#8a8a5c',
+              fontSize: '15.8px',
+              opacity: '0.62',
+            }}
+          >
+            <li style={{ marginRight: '23px' }}>HOME</li>
+            <li style={{ marginRight: '23px' }}>VALORES</li>
+            <li style={{ marginRight: '23px' }}>UNIDADE</li>
+            <li style={{ marginRight: '23px' }}>DIVERSIDADE</li>
+            <li style={{ marginRight: '23px' }}>GOVERNANÇA</li>
+            <li style={{ marginRight: '23px' }}>HISTÓRIA</li>
+            <li style={{ marginRight: '23px' }}>MAIS</li>
+          </ul>
+        </div>
+
+        <hr style={{ marginTop: '-12px' }} />
       </header>
     </div>
   )
